ff5d6d3279e33f54676212ca126e244b5b294517
ashish <ashish.vishwakarma@hexalitics.com>
2024-07-10 19:17:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PDFEditor from "./components/PDFEditor";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100%", height: "100vh" }}>
+      <h1 className="text-3xl font-bold underline">Pdf Render!</h1>
+      <PDFEditor document={"doc.pdf"} />
     </div>
   );
 }
---
+++ src/components/PDFEditor.js
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+// import PSPDFKit from "pspdfkit";
+
+const API_KEY =
+  "sdmp4Jj88n_MhK-Xb8KYhF6wE60mfpx1Sw3v4UFf84l7HTqbAK_2pZd8cbeRteOHTSofh_TQ3IsOYHbRE4FiCFRrUkGXc12i9QxBrH8R7xKGqMte7JXM4lxxGJBsjVoOv5XqIncsBXJtztUrlm4gx6zXdaCruBnRW6QDiJQBNw0TuB_PRT2-2lhE5DpJB_Xb1ogL3uesDP0Mg5HO";
+
+function PDFEditor(props) {
+  const containerRef = useRef(null);
+  useEffect(() => {
+    const container = containerRef.current;
+    let PSPDFKit;
+    (async function () {
+      PSPDFKit = await import("pspdfkit");
+
+      PSPDFKit.unload(container); // Ensure that there's only one PSPDFKit instance.
+
+      await PSPDFKit.load({
+        // Container where PSPDFKit should be mounted.
+        container,
+        // The document to open.
+        document: props.document,
+        // Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
+        baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
+        licenseKey:API_KEY
+      });
+    })();
+
+    return () => PSPDFKit && PSPDFKit.unload(container);
+  }, []);
+
+  return <div ref={containerRef} style={{ width: "100%", height: "100vh" }} />;
+}
+
+export default PDFEditor;
